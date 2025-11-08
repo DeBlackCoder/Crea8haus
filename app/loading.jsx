532c1886a3a54b70f1060767652f97163face008
Hillary Prosper Wahua <hillaryprosperwahua@gmail.com>
2025-11-08 03:49:54 -0800

    Loading

--- app/loading.jsx
+++ app/loading.jsx
@@ -5,7 +5,7 @@ export default function Loading() {
   return (
     <div className="loading-container">
       <div className="loader-circle">
-        <img src="/svg/crea8logo.png" alt="Logo" />
+        
       </div>
       
     </div>
